b9d2da5c80c7679495bcf2cad6a54ef261d88bce
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-02-06 12:00:52 +0100

    "USER REGISTRATION" <This commit contains the SQL script for new data structure> corrected

---
+++ sample_data/askmatepart3-sample-data.sql
@@ -0,0 +1,134 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.6
+-- Dumped by pg_dump version 9.5.6
+
+ALTER TABLE IF EXISTS ONLY public.question DROP CONSTRAINT IF EXISTS pk_question_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.answer DROP CONSTRAINT IF EXISTS pk_answer_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.answer DROP CONSTRAINT IF EXISTS fk_question_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.comment DROP CONSTRAINT IF EXISTS pk_comment_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.comment DROP CONSTRAINT IF EXISTS fk_question_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.comment DROP CONSTRAINT IF EXISTS fk_answer_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS pk_question_tag_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS fk_question_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.tag DROP CONSTRAINT IF EXISTS pk_tag_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS fk_tag_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.answer DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.question DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.comment DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+
+DROP TABLE IF EXISTS public.question_tag;
+DROP SEQUENCE IF EXISTS public.question_tag_seq;
+DROP TABLE IF EXISTS public.tag;
+DROP SEQUENCE IF EXISTS public.tag_seq;
+
+DROP TABLE IF EXISTS public.question;
+DROP SEQUENCE IF EXISTS public.question_id_seq;
+CREATE TABLE question (
+    id serial NOT NULL,
+    submission_time timestamp without time zone,
+    view_number integer,
+    vote_number integer,
+    title text,
+    message text,
+    image text
+);
+
+DROP TABLE IF EXISTS public.answer;
+DROP SEQUENCE IF EXISTS public.answer_id_seq;
+CREATE TABLE answer (
+    id serial NOT NULL,
+    submission_time timestamp without time zone,
+    vote_number integer,
+    question_id integer,
+    message text,
+    image text
+);
+
+DROP TABLE IF EXISTS public.comment;
+DROP SEQUENCE IF EXISTS public.comment_id_seq;
+CREATE TABLE comment (
+    id serial NOT NULL,
+    question_id integer,
+    answer_id integer,
+    message text,
+    submission_time timestamp without time zone,
+    edited_count integer
+);
+
+DROP TABLE IF EXISTS public.users;
+DROP SEQUENCE IF EXISTS public.users_id_seq;
+CREATE TABLE users
+(
+	id serial
+		constraint users_pk
+			primary key,
+	user_name varchar(100) NOT NULL,
+	salt varchar(128) NOT NULL,
+	hashed_password text NOT NULL,
+	reg_date timestamp NOT NULL
+);
+
+CREATE UNIQUE INDEX users_user_name_uindex
+	on users (user_name);
+
+
+ALTER TABLE ONLY answer
+    ADD CONSTRAINT pk_answer_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY comment
+    ADD CONSTRAINT pk_comment_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY question
+    ADD CONSTRAINT pk_question_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY comment
+    ADD CONSTRAINT fk_answer_id FOREIGN KEY (answer_id) REFERENCES answer(id);
+
+ALTER TABLE ONLY answer
+    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id);
+
+ALTER TABLE ONLY comment
+    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id);
+
+ALTER TABLE answer
+	ADD user_id int;
+
+ALTER TABLE answer
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE question
+	ADD user_id int;
+
+ALTER TABLE question
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE comment
+	ADD user_id int;
+
+ALTER TABLE comment
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+
+INSERT INTO question VALUES (0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL);
+INSERT INTO question VALUES (1, '2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
+I could easy managing the loading order with wp_enqueue_script so first I load jquery then I load booklet so everything is fine.
+BUT in my theme i also using jquery via webpack so the loading order is now following:
+jquery
+booklet
+app.js (bundled file with webpack, including jquery)', 'images/image1.png');
+INSERT INTO question VALUES (2, '2017-05-01 10:41:00', 1364, 57, 'Drawing canvas with an image picked with Cordova Camera Plugin', 'I''m getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I''m on IOS, it throws errors such as cross origin issue, or that I''m trying to use an unknown format.
+', NULL);
+SELECT pg_catalog.setval('question_id_seq', 2, true);
+
+INSERT INTO answer VALUES (1, '2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', NULL);
+INSERT INTO answer VALUES (2, '2017-04-25 14:42:00', 35, 1, 'Look it up in the Python docs', 'images/image2.jpg');
+SELECT pg_catalog.setval('answer_id_seq', 2, true);
+
+INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00');
+INSERT INTO comment VALUES (2, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00');
+SELECT pg_catalog.setval('comment_id_seq', 2, true);
+
+INSERT INTO "public"."users" ("id", "user_name", "salt", "hashed_password", "reg_date")
+VALUES (0, 'John McClain', 'b''$2b$12$JqB2dSEoJMBxchKv/hwTyu''', 'b''$2b$12$IsTvK4Typtc8bURN7opczODUoLubN5z9mY6rSmne8TKYbqUGp7H0S''', '2019-02-06 09:32:47.657000') 
