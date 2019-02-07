49290efc429813e02a4f0654cb04874fb7b18c69
Petrav <vagi.p94@gmail.com>
2019-02-07 13:55:49 +0100

    "REGISTER": fixing bugs

--- util.py
+++ util.py
@@ -1,11 +1,6 @@
 import bcrypt
 
 
-def generate_salt():
-    salt = bcrypt.gensalt()
-    return salt
-
-
-def hash_password(plain_text_password, salt):
-    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), salt)
+def hash_password(plain_text_password):
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
     return hashed_bytes.decode('utf-8')
